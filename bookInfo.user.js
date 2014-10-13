94818e1cfb0b37a779765522221bbaf6cb621b41
Karius <kittarius@gmail.com>
2014-10-13 20:17:52 +0800

    first commit;

---
+++ bookInfo.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name        bookInfo
+// @namespace   bookinfo.guide.next
+// @description 在每个作品具体内容页面显示DMM番号和发行商信息
+// @include     http://item.jd.com/*
+// @version     1.0
+// @grant       none
+// @updateURL   https://github.com/Karius/bookInfo/raw/master/bookInfo.user.js
+// @downloadURL https://github.com/Karius/bookInfo/raw/master/bookInfo.user.js
+// @require     http://code.jquery.com/jquery-1.9.1.min.js
+// ==/UserScript==
+
+
