b4dcc96d239b1e2677a95245f5e69079b7912a7c
Karius <kittarius@gmail.com>
2014-10-13 20:22:16 +0800

    摘取京东的书籍信息;

--- bookInfo.user.js
+++ bookInfo.user.js
@@ -10,4 +10,34 @@
 // @require     http://code.jquery.com/jquery-1.9.1.min.js
 // ==/UserScript==
 
+// 作者名列表
+var authLst = "";
+$("div#product-authorinfo a").each(function (index, element) {
+authLst += $(element).text ().replace (/ /g, "") + ",";
+});
+authLst = authLst.replace (/,$/g, ""); // 作者列表
 
+// 书名
+var bookName = $("div#name h1").text ().replace (/[\s]*?$/, "");
+
+// 原价
+var orgPrice = $("div.dd em s").text ().replace (/[^\d]*/, "")
+
+// 打折价
+var price = $("div.dd strong").text ().replace (/[^\d]*/, "");
+
+// ISBN
+var ISBN = $("ul.detail-list li.fore4").text ().replace (/^[A-Za-z]*：/, "");
+
+// 出版社
+var press = $("ul.detail-list li.fore2 a").text ();
+
+// 开本
+var folio = $("ul.detail-list li.fore6").text ().replace (/^[^\d]*/, "").replace (/[^\d]*?$/, "");
+
+// 页数
+var pages = $("ul.detail-list li.fore7").text ().replace (/^[^\d]*/, "");
+
+// 出版时间
+var dateOfPublication = $("ul.detail-list li.fore9").text ().replace (/^[^\d]*/, "");
+alert (dateOfPublication);
